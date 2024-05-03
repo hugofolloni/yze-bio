20292a0a9b8cbbd5f2e61032590598a6258eabf6
hugofolloni <hugofollogua07@gmail.com>
2024-05-02 23:03:50 -0300

    Adding links to profile and fetching specific data from user

--- website/src/Card.js
+++ website/src/Card.js
@@ -1,51 +1,60 @@
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Curtain from './Curtain';
+import { Basic } from "./Layout"
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+    background-color: ${props => props.pageBackgroundColor};
+    position: relative;
+    z-index: 1;
+`
+
+const CardDiv = styled.div`
+    display: flex;
+    width: 700px;
+    height: 400px;
+    z-index: 2;
+`
 
 const Card = () => {
 
-    const [borderRadius, setBorderRadius] = useState('0')
-    const [backgroundColor, setBackgroundColor] = useState('black')
+    const username = window.location.href.split('3000/')[1]
+
+    const [data, setData] = useState({status: 999})
+    const [pageBackgroundColor, setPageBackgroundColor] = useState("#eaeaea")
 
     useEffect(() => {
-        fetch('https://localhost:7041/api/Layout')
+        fetch(`https://localhost:7041/api/User/user-with-layout?nickname=${username}`)
         .then(res => res.json())
         .then(data => {
-            const item = data[0]
-            setBorderRadius(item.borderRadius);
-            setBackgroundColor(item.backgroundColor);
+            console.log(data)
+            if(data.status !== 404){
+                console.log(data.title)
+                setPageBackgroundColor(data.layout.pageBackgroundColor)
+                setData(data);
+                console.log(data)
+            }
         });
-    })
+    }, [username])
 
-    const Wrapper = styled.div`
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: center;
-        width: 100vw;
-        height: 100vh;
-        background-color: #eaeaea;
-        position: relative;
-        z-index: 1;
-    `
-
-    const CardDiv = styled.div`
-        display: flex;
-        align-items: center;
-        width: 700px;
-        height: 400px;
-        background-color: ${backgroundColor};
-        border-radius: ${borderRadius}px;
-        z-index: 2;
-    `
 
     return ( 
-        <Wrapper className="card-wrapper">
+        <Wrapper pageBackgroundColor={pageBackgroundColor}>
             <Curtain />
-            <CardDiv className='card-div'>
-                <a href="https://hugofolloni.netlify.app" target='_blank' rel='noreferrer'>Hugo</a>
-                🚀
-            </CardDiv>
+            {(data.status !== 404 && data.status !== 999 && 
+                <CardDiv>
+                    <Basic data={data}/>
+                </CardDiv>) || 
+                <div className="sorry">
+                    <span>Sorry, this card does not exist.</span>
+                </div>
+            }
         </Wrapper>
      );
 }
---
+++ website/src/Layout.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+const LayoutStyled = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    align-items: flex-start;
+    justify-content: flex-start;
+    padding: 20px;
+    background-color: ${props => props.layout.cardBackgroundColor};
+    border-radius: ${props => props.layout.borderRadius}px;
+`
+
+const Title = styled.span`
+    font-size: 32px;
+    font-family: ${props => props.layout.fontFamily};
+    color: ${props => props.layout.fontColor};
+    font-weight: 700;
+    margin-bottom: 10px;
+`
+
+const Subtitle = styled.span`
+    margin-bottom: 20px;
+    font-weight: 600;
+`
+
+const Description = styled.span``
+
+const Basic = (props) => {
+
+    console.log(props.data.layout)
+    return (  
+        <LayoutStyled layout={props.data.layout}>
+            <Title layout={props.data.layout}>{props.data.title}</Title>
+            <Subtitle layout={props.data.layout}>{props.data.subtitle}</Subtitle>
+            <Description layout={props.data.layout}>{props.data.description}</Description>
+        </LayoutStyled>
+        );
+}
+ 
+export { Basic };
